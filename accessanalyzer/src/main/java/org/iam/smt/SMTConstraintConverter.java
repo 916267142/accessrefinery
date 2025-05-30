cd1956d71f90a41b1d771ec5d39787c578ef8c2b
after-the-end <2519763165@qq.com>
2025-05-31 03:38:52 +0800

    Delete3

--- accessanalyzer/src/main/java/org/iam/smt/SMTConstraintConverter.java
+++
@@ -1,7 +0,0 @@
-package org.iam.smt;
-
-import org.iam.exceptions.SolverRuntimeException;
-
-public interface SMTConstraintConverter<T, R extends Request, I>{
-    T toSMTConstraint(R request, I input) throws SolverRuntimeException;
-}
