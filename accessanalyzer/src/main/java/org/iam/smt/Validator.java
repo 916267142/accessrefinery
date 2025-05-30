ca3e93e37c8e397061554d0e1667ac631663e658
after-the-end <2519763165@qq.com>
2025-05-31 04:14:05 +0800

    Delete4

--- accessanalyzer/src/main/java/org/iam/smt/Validator.java
+++
@@ -1,6 +0,0 @@
-package org.iam.smt;
-
-public interface Validator {
-    boolean checkImplication(Object objectP, Object objectQ);
-    boolean checkIntersection(Object objectP, Object objectQ);
-}
